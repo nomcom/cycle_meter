1db40981b4da6f719340f5ecc9b2f85f076d5109
Satoshi Nomura <s-nomura@climb-net.co.jp>
2023-03-09 10:34:08 +0900

    add start

--- backend/tool/dev_init.sh
+++ backend/tool/dev_init.sh
@@ -3,6 +3,8 @@
 # exit on error
 set -o errexit
 
+npm install
+
 ROOT_PATH=./prisma
 npx prisma db push --schema=$ROOT_PATH/dev/schema.prisma
 npx prisma db push --schema=$ROOT_PATH/dev/schema_append.prisma
