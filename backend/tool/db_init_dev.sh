93d401318de1baf0d5ffbcadd76a663323039bcd
Satoshi Nomura <s-nomura@climb-net.co.jp>
2023-03-09 10:13:43 +0900

    render 用 sqliteのみ(修正)

--- backend/tool/db_init_dev.sh
+++ backend/tool/db_init_dev.sh
@@ -4,5 +4,5 @@
 set -o errexit
 
 ROOT_PATH=./prisma
-npx prisma db push --schema=$ROOT_PATH/schema.prisma
-npx prisma db push --schema=$ROOT_PATH/schema_append.prisma
+npx prisma db push --schema=$ROOT_PATH/dev/schema.prisma
+npx prisma db push --schema=$ROOT_PATH/dev/schema_append.prisma
