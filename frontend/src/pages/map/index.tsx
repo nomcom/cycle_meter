f443de1b2cc00ac97eb508a5d24f1e8bb75c2ea0
Satoshi Nomura <s-nomura@climb-net.co.jp>
2023-04-12 14:15:09 +0900

    [FE]change google api module

--- frontend/src/pages/map/index.tsx
+++ frontend/src/pages/map/index.tsx
@@ -1,27 +1,11 @@
-/*
- * Copyright 2021 Google LLC. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import * as React from "react";
-import { Wrapper, Status } from "@googlemaps/react-wrapper";
-import { Polyline } from "@react-google-maps/api";
+import {
+  Polyline,
+  Marker,
+  GoogleMap,
+  LoadScript,
+} from "@react-google-maps/api";
 import { api } from "common-library";
-
-const render = (status: Status) => {
-  return <h1>{status}</h1>;
-};
 
 const App: React.FC = () => {
   const [positions, setPositions] = React.useState<google.maps.LatLng[]>([]);
@@ -33,14 +17,6 @@ const App: React.FC = () => {
 
   const onClick = (e: google.maps.MapMouseEvent) => {
     // avoid directly mutating state
-  };
-
-  const onIdle = (m: google.maps.Map) => {
-    console.log("onIdle");
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    setZoom(m.getZoom()!);
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    setCenter(m.getCenter()!.toJSON());
   };
 
   const onSubmit = React.useCallback(async () => {
@@ -97,13 +73,12 @@ const App: React.FC = () => {
 
   return (
     <div style={{ display: "flex", height: "100%" }}>
-      <Wrapper apiKey={import.meta.env.VITE_GOOGLE_MAP_API_KEY} render={render}>
-        <Map
+      <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAP_API_KEY}>
+        <GoogleMap
           center={center}
           onClick={onClick}
-          onIdle={onIdle}
           zoom={zoom}
-          style={{ flexGrow: "1", height: "100%" }}
+          mapContainerStyle={{ flexGrow: "1", height: "100%" }}
         >
           {positions.map((latLng, i) => (
             <Marker key={i} position={latLng} />
@@ -113,7 +88,7 @@ const App: React.FC = () => {
             options={{
               strokeColor: "#ff2527",
               strokeOpacity: 0.75,
-              strokeWeight: 2,
+              strokeWeight: 5,
               // icons: [
               //   {
               //     icon: lineSymbol,
@@ -123,110 +98,12 @@ const App: React.FC = () => {
               // ],
             }}
           />
-        </Map>
-      </Wrapper>
+        </GoogleMap>
+      </LoadScript>
       {/* Basic form for controlling center and zoom of map. */}
       {form}
     </div>
   );
 };
-interface MapProps extends google.maps.MapOptions {
-  style: { [key: string]: string };
-  onClick?: (e: google.maps.MapMouseEvent) => void;
-  onIdle?: (map: google.maps.Map) => void;
-  children?: React.ReactNode;
-}
-
-const Map: React.FC<MapProps> = ({
-  onClick,
-  onIdle,
-  children,
-  style,
-  ...options
-}) => {
-  const ref = React.useRef<HTMLDivElement>(null);
-  const [map, setMap] = React.useState<google.maps.Map>();
-
-  React.useEffect(() => {
-    if (ref.current && !map) {
-      setMap(new window.google.maps.Map(ref.current, {}));
-    }
-  }, [ref, map]);
-
-  // because React does not do deep comparisons, a custom hook is used
-  // see discussion in https://github.com/googlemaps/js-samples/issues/946
-  useDeepCompareEffectForMaps(() => {
-    if (map) {
-      map.setOptions(options);
-    }
-  }, [map, options]);
-
-  React.useEffect(() => {
-    if (map) {
-      ["click", "idle"].forEach((eventName) =>
-        google.maps.event.clearListeners(map, eventName)
-      );
-
-      if (onClick) {
-        map.addListener("click", onClick);
-      }
-
-      if (onIdle) {
-        map.addListener("idle", () => onIdle(map));
-      }
-    }
-  }, [map, onClick, onIdle]);
-
-  return (
-    <>
-      <div ref={ref} style={style} />
-      {React.Children.map(children, (child) => {
-        if (React.isValidElement(child)) {
-          // set the map prop on the child component
-          return React.cloneElement(child, { map } as any);
-        }
-      })}
-    </>
-  );
-};
-
-const Marker: React.FC<google.maps.MarkerOptions> = (options) => {
-  const [marker, setMarker] = React.useState<google.maps.Marker>();
-
-  React.useEffect(() => {
-    if (!marker) {
-      setMarker(new google.maps.Marker());
-    }
-
-    // remove marker from map on unmount
-    return () => {
-      if (marker) {
-        marker.setMap(null);
-      }
-    };
-  }, [marker]);
-
-  React.useEffect(() => {
-    if (marker) {
-      marker.setOptions(options);
-    }
-  }, [marker, options]);
-
-  return null;
-};
-function useDeepCompareMemoize(value: any) {
-  const ref = React.useRef();
-
-  ref.current = value;
-
-  return ref.current;
-}
-
-function useDeepCompareEffectForMaps(
-  callback: React.EffectCallback,
-  dependencies: any[]
-) {
-  React.useEffect(callback, dependencies.map(useDeepCompareMemoize));
-}
 
 export default App;
