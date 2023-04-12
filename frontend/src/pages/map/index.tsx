f4a219174663a476e0b27f45607863d193b0c578
Satoshi Nomura <s-nomura@climb-net.co.jp>
2023-04-12 17:40:53 +0900

    [FE]google undefined bug fix

--- frontend/src/pages/map/index.tsx
+++ frontend/src/pages/map/index.tsx
@@ -3,11 +3,16 @@ import {
   Polyline,
   Marker,
   GoogleMap,
-  LoadScript,
+  useLoadScript,
 } from "@react-google-maps/api";
 import { api } from "common-library";
 
 const App: React.FC = () => {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAP_API_KEY, // ,
+    // ...otherOptions
+  });
+  const [map, setMap] = React.useState<any>(null);
   const [positions, setPositions] = React.useState<google.maps.LatLng[]>([]);
   const [zoom, setZoom] = React.useState(12); // initial zoom
   const [center, setCenter] = React.useState<google.maps.LatLngLiteral>({
@@ -15,10 +20,21 @@ const App: React.FC = () => {
     lng: 0,
   });
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const onClick = (e: google.maps.MapMouseEvent) => {
-    // avoid directly mutating state
-  };
+  const onLoad = React.useCallback(function callback(map: any) {
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(function callback(map: any) {
+    setMap(null);
+  }, []);
+  const onZoomChanged = React.useCallback(
+    function callback() {
+      if (map && isLoaded) {
+        setZoom(map.getZoom());
+      }
+    },
+    [map]
+  );
 
   const onSubmit = React.useCallback(async () => {
     const response = await api.markersGet({
@@ -40,7 +56,42 @@ const App: React.FC = () => {
       setPositions(positions.concat(newPos));
       setCenter(newPos[newPos.length - 1].toJSON());
     }
-  }, [api.markerCreate]);
+  }, [positions]);
+
+  if (!isLoaded) {
+    return <></>;
+  }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const onClick = (e: google.maps.MapMouseEvent) => {
+    // avoid directly mutating state
+  };
+
+  let mapElements = null;
+  // ロード済みの場合のみ
+  mapElements = (
+    <>
+      {positions.map((latLng, i) => (
+        <Marker key={i} position={latLng} />
+      ))}
+      <Polyline
+        path={positions}
+        options={{
+          strokeColor: "#ff2527",
+          strokeOpacity: 0.75,
+          strokeWeight: 5,
+          // icons: [
+          //   {
+          //     icon: {
+          //       path: google.maps.SymbolPath.CIRCLE,
+          //     },
+          //     offset: "0",
+          //     repeat: "20px",
+          //   },
+          // ],
+        }}
+      />
+    </>
+  );
 
   const form = (
     <div
@@ -66,7 +117,7 @@ const App: React.FC = () => {
       <label htmlFor="lng">Longitude</label>
       <input type="number" id="lng" name="lng" value={center.lng} disabled />
 
-      <button title="Submit" onClick={onSubmit}>
+      <button title="Submit" onClick={onSubmit} disabled={!isLoaded}>
         データ取得
       </button>
     </div>
@@ -74,34 +125,17 @@ const App: React.FC = () => {
 
   return (
     <div style={{ display: "flex", height: "100%" }}>
-      <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAP_API_KEY}>
-        <GoogleMap
-          center={center}
-          onClick={onClick}
-          zoom={zoom}
-          mapContainerStyle={{ flexGrow: "1", height: "100%" }}
-        >
-          {positions.map((latLng, i) => (
-            <Marker key={i} position={latLng} />
-          ))}
-          <Polyline
-            path={positions}
-            options={{
-              strokeColor: "#ff2527",
-              strokeOpacity: 0.75,
-              strokeWeight: 5,
-              // icons: [
-              //   {
-              //     icon: lineSymbol,
-              //     offset: "0",
-              //     repeat: "20px",
-              //   },
-              // ],
-            }}
-          />
-        </GoogleMap>
-      </LoadScript>
-      {/* Basic form for controlling center and zoom of map. */}
+      <GoogleMap
+        center={center}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+        onClick={onClick}
+        onZoomChanged={onZoomChanged}
+        zoom={zoom}
+        mapContainerStyle={{ flexGrow: "1", height: "100%" }}
+      >
+        {mapElements}
+      </GoogleMap>
       {form}
     </div>
   );
