4c4b4e88f02fac7571af8e933c74d47ad0e2f44f
Satoshi Nomura <s-nomura@climb-net.co.jp>
2023-04-17 15:13:51 +0900

    [mobile]ソフトキーボードが画面を圧迫しないように

--- mobile/gps_logger/app.config.ts
+++ mobile/gps_logger/app.config.ts
@@ -1,55 +1,55 @@
 module.exports = {
-  "expo": {
-    "name": "gps_logger",
-    "slug": "gps_logger",
-    "version": "1.0.0",
-    "orientation": "portrait",
-    "icon": "./assets/icon.png",
-    "userInterfaceStyle": "light",
-    "splash": {
-      "image": "./assets/splash.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#ffffff"
+  expo: {
+    name: "gps_logger",
+    slug: "gps_logger",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
     },
-    "assetBundlePatterns": [
-      "**/*"
-    ],
-    "ios": {
-      "supportsTablet": true
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true,
     },
-    "android": {
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/adaptive-icon.png",
-        "backgroundColor": "#ffffff"
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff",
       },
-      "permissions": [
+      permissions: [
         "ACCESS_BACKGROUND_LOCATION",
         "ACCESS_COARSE_LOCATION",
         "ACCESS_FINE_LOCATION",
-        "FOREGROUND_SERVICE"
+        "FOREGROUND_SERVICE",
       ],
-      "package": "com.nomurast.myapptemp",
-      "config" : {
-        "googleMaps": {
-          "apiKey": process.env.GOOGLEMAP_ANDROID_API_KEY
-        }
-      }
+      package: "com.nomurast.myapptemp",
+      config: {
+        googleMaps: {
+          apiKey: process.env.GOOGLEMAP_ANDROID_API_KEY,
+        },
+      },
+      softwareKeyboardLayoutMode: "pan",
     },
-    "web": {
-      "favicon": "./assets/favicon.png"
+    web: {
+      favicon: "./assets/favicon.png",
     },
-    "plugins": [
+    plugins: [
       [
         "expo-location",
         {
-          "locationAlwaysAndWhenInUsePermission": "Allow $(PRODUCT_NAME) to use your location."
-        }
-      ]
+          locationAlwaysAndWhenInUsePermission:
+            "Allow $(PRODUCT_NAME) to use your location.",
+        },
+      ],
     ],
-    "extra": {
-      "eas": {
-        "projectId": "5fa0eab4-ce91-4018-8a35-8bc4c68ecda1"
-      }
-    }
-  }
-}
+    extra: {
+      eas: {
+        projectId: "5fa0eab4-ce91-4018-8a35-8bc4c68ecda1",
+      },
+    },
+  },
+};
