f3a07233f48d68287b345a20bd259087d0fc0541
Satoshi Nomura <s-nomura@climb-net.co.jp>
2023-04-18 17:51:15 +0900

    [FE]change firebaseConfig

--- frontend/src/firebaseConfig.ts
+++ frontend/src/firebaseConfig.ts
@@ -15,13 +15,13 @@ if (typeof atob === "undefined") {
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  databaseURL: import.meta.env.VITE_DATABASEURL,
-  projectId: import.meta.env.VITE_PROJECTID,
+  // apiKey: import.meta.env.VITE_APIKEY,
+  // authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  // databaseURL: import.meta.env.VITE_DATABASEURL,
+  // projectId: import.meta.env.VITE_PROJECTID,
   storageBucket: import.meta.env.VITE_STORAGEBUCKET,
   // messagingSenderId: 'sender-id',
-  appId: import.meta.env.VITE_APPID,
+  // appId: import.meta.env.VITE_APPID,
   // measurementId: 'G-measurement-id',
 };
 
