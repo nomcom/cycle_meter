6367ce7f960a8141a53e02481064836089b6b74d
Satoshi Nomura <s-nomura@climb-net.co.jp>
2023-04-18 17:38:31 +0900

    [mobile]add github action5

--- frontend/src/firebaseConfig.ts
+++ frontend/src/firebaseConfig.ts
@@ -1,9 +1,9 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
-import { decode } from 'base-64';
+import { decode } from "base-64";
 
-if (typeof atob === 'undefined') {
-    global.atob = decode;
+if (typeof atob === "undefined") {
+  global.atob = decode;
 }
 
 // Optionally import the services that you want to use
@@ -15,14 +15,14 @@ if (typeof atob === 'undefined') {
 
 // Initialize Firebase
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_APIKEY,
-    authDomain: import.meta.env.VITE_AUTHDOMAIN,
-    databaseURL: import.meta.env.VITE_DATABASEURL,
-    projectId: import.meta.env.VITE_PROJECTID,
-    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-    // messagingSenderId: 'sender-id',
-    appId: import.meta.env.VITE_APPID,
-    // measurementId: 'G-measurement-id',
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASEURL,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  // messagingSenderId: 'sender-id',
+  appId: import.meta.env.VITE_APPID,
+  // measurementId: 'G-measurement-id',
 };
 
 const app = initializeApp(firebaseConfig);
@@ -32,4 +32,4 @@ const app = initializeApp(firebaseConfig);
 // Initialize Cloud Storage and get a reference to the service
 const storage = getStorage(app);
 
-export { storage }
+export { storage };
